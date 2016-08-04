c99c00dd6151022b7160c7b70c8dc2f9e3490b54
Evgeniy Leontiev <potehin@gmail.com>
2016-08-04 12:13:06 +0400

    cover tender not find on mailer with fake tender

--- app/mailers/tender_mailer.rb
+++ app/mailers/tender_mailer.rb
@@ -3,6 +3,9 @@ class TenderMailer < ApplicationMailer
 
   def invitation(tender = nil)
     @tender = tender
+    if @tender == nil
+        @tender = tender_not_find
+    end
     @url  = 'http://example.com/login'
     mail(to: "e_leontiev@24tender.ru", subject: 'Test Invites to Tenders')
   end
@@ -17,5 +20,19 @@ class TenderMailer < ApplicationMailer
 
   def something_changes_notice
 
+  end
+
+private
+
+  def tender_not_find
+    Tender.new({data:{
+      'Link' => 'http://24tender.ru',
+      'Id' => "404, NOT FOUND",
+      'lots' => {
+          'lot' => {
+              'customer' => "404, NOT FOUND"
+            }
+        }
+      }})
   end
 end
