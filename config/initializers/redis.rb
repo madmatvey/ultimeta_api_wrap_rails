b398e16f517d19f2c605cec49c604faba56d7cb5
Evgeniy Leontiev <potehin@gmail.com>
2016-08-10 12:10:32 +0400

    fix redis initializer

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
 uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )
-REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+REDIS = Redis.new(:url => uri, :host => uri.host, :port => uri.port, :password => uri.password)
