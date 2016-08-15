af507886b5f780b35a6102c8a3246b113b33a447
Evgeniy Leontiev <potehin@gmail.com>
2016-08-15 20:21:48 +0400

    try to fix redis connection7

--- config/initializers/redis_store.rb
+++ config/initializers/redis_store.rb
@@ -1,3 +1,4 @@
-redis_url = ENV["REDISTOGO_URL"] || "redis://127.0.0.1:6379/0/myapp"
-UltimetaApiWrapRails::Application.config.cache_store = :redis_store, redis_url
-UltimetaApiWrapRails::Application.config.session_store :redis_store, redis_server: redis_url
+uri = ENV["REDISTOGO_URL"] || "redis://localhost:6379/"
+# REDIS = Redis.new(:url => uri)
+UltimetaApiWrapRails::Application.config.cache_store = :redis_store, uri
+UltimetaApiWrapRails::Application.config.session_store :redis_store, redis_server: uri
