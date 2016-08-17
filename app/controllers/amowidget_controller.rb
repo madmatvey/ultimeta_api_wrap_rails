9a12f5b6f62b55a2a7bf1ed83c3b732375c4541b
Evgeniy Leontiev <potehin@gmail.com>
2016-08-17 15:42:17 +0400

    add access headers response to amowodget

--- app/controllers/amowidget_controller.rb
+++ app/controllers/amowidget_controller.rb
@@ -30,5 +30,9 @@ class AmowidgetController < ApplicationController
     def allow_amo_iframe
       response.headers.except! 'X-Frame-Options'
       response.headers['X-XSS-Protection'] = "0"
+      response.headers['Access-Control-Allow-Origin'] = '*'
+      response.headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+      response.headers['Access-Control-Request-Method'] = '*'
+      response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     end
 end
